ff3a50a2bfee7ec21ed2a28c3f972f78d1b3e492
Abadur Rahman <abadur391@gmail.com>
2024-08-29 18:00:49 +0600

    WatchList Feature

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// TODO - Pagination, Watchlist, Search, Filter & LocalStorage
+// TODO - Pagination, Watchlist, Filter & LocalStorage
 // TODO - Project Technical Description
 
 // Api Key
@@ -11,7 +11,7 @@ let resData = [];
 async function getMarketData() {
   try {
     const response = await fetch(
-      "https://api.mobula.io/api/1/all?fields=price&fields=logo&fields=price_change_24h&fields=price_change_7d",
+      "https://api.mobula.io/api/1/market/query?limit=10&fields=price&fields=logo&fields=price_change_24h&fields=price_change_7d",
       {
         headers: {
           Authorization: `${key}`,
@@ -22,7 +22,7 @@ async function getMarketData() {
       throw new Error(`Response status: ${response.status}`);
     }
     const data = await response.json();
-    resData = data.data;
+    resData = data;
     renderData(resData);
   } catch (error) {
     console.error(error.message);
@@ -30,7 +30,6 @@ async function getMarketData() {
 }
 
 getMarketData();
-
 
 // Rendering Data
 function renderData(resData) {
@@ -42,26 +41,33 @@ function renderData(resData) {
     const tableRow = document.createElement("tr");
     tableRow.className = "even:dark:bg-neutral-800 bg-neutral-900";
 
+    // Count Col
     const countTd = document.createElement("td");
     countTd.className = "px-6 py-4 max-w-2";
     count++;
     countTd.innerText = count;
     tableRow.appendChild(countTd);
 
+    // Name Col
     const nameTd = document.createElement("td");
-    nameTd.className = "px-6 py-4 text-wrap max-w-4";
+    nameTd.className = "px-6 py-4 text-wrap flex items-center";
     const logoImg = document.createElement("img");
     logoImg.src = coin.logo;
     logoImg.className = "w-6 h-6 mr-2";
-    nameTd.innerText = coin.name;
+
     nameTd.appendChild(logoImg);
+    const nameText = document.createTextNode(coin.name);
+    nameTd.appendChild(nameText);
+
     tableRow.appendChild(nameTd);
 
+    // Price Col
     const priceTd = document.createElement("td");
     priceTd.className = "px-6 py-4 text-wrap";
-    priceTd.innerText = `$${coin.price}`;
+    priceTd.innerText = `$${coin.price.toFixed(4)}`;
     tableRow.appendChild(priceTd);
 
+    // Day Change Col
     const dayChange = document.createElement("td");
     dayChange.className = "px-6 py-4 text-wrap";
     const badge1 = document.createElement("span");
@@ -72,10 +78,11 @@ function renderData(resData) {
       badge1.className =
         "font-medium px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300";
     }
-    badge1.innerText = coin.price_change_24h;
+    badge1.innerText = coin.price_change_24h.toFixed(2);
     dayChange.appendChild(badge1);
     tableRow.appendChild(dayChange);
 
+    // Week Change Col
     const weekChange = document.createElement("td");
     weekChange.className = "px-6 py-4 text-wrap";
     const badge2 = document.createElement("span");
@@ -86,9 +93,24 @@ function renderData(resData) {
       badge2.className =
         "font-medium px-2.5 py-0.5 rounded dark:bg-red-900 dark:text-red-300";
     }
-    badge2.innerText = coin.price_change_7d;
+    badge2.innerText = coin.price_change_7d.toFixed(2);
     weekChange.append(badge2);
     tableRow.appendChild(weekChange);
+
+    // Watchlist Col
+    const watchList = document.createElement("input");
+    watchList.type = "checkbox";
+    watchList.id = "watchlistbox";
+    watchList.className =
+      " w-4 h-4 text-orange-600 bg-neutral-100 border-gray-300 rounded  dark:focus:ring-orange-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-neutral-700 dark:border-gray-60";
+    watchList.addEventListener("change", (e) => {
+      if (e.target.checked) {
+        addToWatchList(coin);
+      } else {
+        removeFromWatchList(coin)
+      }
+    });
+    tableRow.appendChild(watchList);
 
     tableBody.appendChild(tableRow);
   }
@@ -97,10 +119,32 @@ function renderData(resData) {
 // Searching Data
 searchBar.addEventListener("keyup", (e) => {
   const query = e.target.value.toLowerCase();
-  console.log(query)
   let searchedData = resData.filter((coin) => {
     return coin.name.toLowerCase().includes(query);
-  })
-  renderData(searchedData)
+  });
+  renderData(searchedData);
 });
 
+let watchList = [];
+function addToWatchList(coin) {
+  watchList.push(coin)
+  // console.log(watchList)
+}
+
+// Remove from Watchlist
+function removeFromWatchList(coin){
+  watchList.pop(coin)
+  // console.log(watchList)
+}
+
+// Render Watch List
+function renderWatchList(){
+  const watchListContainer = document.getElementById('watchlist-container');
+  const coinCard = document.createElement('div');
+  coinCard.className= "max-w-sm border rounded-lg shadow dark:bg-gray-800 dark:border-gray-700";
+  const coinLogo = document.createElement('img');
+  coinLogo.className="rounded-t-lg";
+  watchListContainer.appendChild(coinCard)
+}
+
+renderWatchList()
